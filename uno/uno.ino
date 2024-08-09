d2bf222ef627749e830aedf25cafa9c977231490
임원정 <oddish0513@gmail.com>
2024-08-09 16:51:47 +0200

    feat: send rpm to rasp #8

--- uno/uno.ino
+++ uno/uno.ino
@@ -1,42 +1,58 @@
-// for receiver
-
 #include <SPI.h>
-#include <mcp2515_can.h>
-const int SPI_CS_PIN = 9;
-mcp2515_can CAN_RECEIVE(SPI_CS_PIN); // Set CS pin
+#include "mcp2515_can.h"
+
+#define sensor 3
+const int SPI_CS_PIN = 9;     // default is 9
+mcp2515_can CAN(SPI_CS_PIN);  // Set CS pin
+
+
+volatile unsigned int pulseCount = 0;
+unsigned long currentMillis = 0;
+unsigned long lastMillis = 0;
+
+unsigned char data[8];
+
 void setup() {
-  Serial.begin(9600);
-  while (CAN_OK != CAN_RECEIVE.begin(CAN_500KBPS)) { // init can bus : baudrate = 500k
-    Serial.println("CAN_RECEIVE init fail, retry...");
-    delay(100);
+  SERIAL_PORT_MONITOR.begin(115200);
+
+  // init can bus : baudrate = 500k always keep it same to the Rpi CAN interface
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {
+      SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
+      delay(100);
   }
-  Serial.println("CAN_RECEIVE init ok!");
+  SERIAL_PORT_MONITOR.println("CAN init ok!");
+
+  pinMode(sensor, INPUT);
+  attachInterrupt(digitalPinToInterrupt(sensor), countPulses, RISING);
 }
 
-
 void loop() {
-  unsigned char len = 0;
-  unsigned char buf[8];
+  
+  currentMillis = millis();
 
-  if (CAN_MSGAVAIL == CAN_RECEIVE.checkReceive()) { // Check for received data
-    CAN_RECEIVE.readMsgBuf(&len, buf); // Read data: len = data length, buf = data byte(s)
-    // Print message ID and length
-    Serial.print("ID: ");
-    Serial.print(CAN_RECEIVE.getCanId(), HEX);
-    Serial.print("  Data Length: ");
-    Serial.println(len);
-    // Print each byte of the data
-    // Serial.print("  Frame: ");
-    // for (int i = 0; i < len; i++) {
-    //   Serial.print(buf[i], HEX);
-    //   Serial.print("\t");
-    // }
-    float receivedSpeed;
-    memcpy(&receivedSpeed, buf, sizeof(receivedSpeed));
+  if (currentMillis - lastMillis >= 1000) {
 
-    Serial.println(receivedSpeed);
+    lastMillis = currentMillis;
 
-    Serial.println();
-    Serial.println("-------------------------------");
+    float rpm = getRPM();
+    Serial.println(rpm);
+
+    pulseCount = 0;
+    memcpy(data, &rpm, sizeof(rpm));
+    CAN.sendMsgBuf(0x7, 0, 8, data); // Send the data on the CAN bus
+
+    SERIAL_PORT_MONITOR.println("CAN BUS sendMsgBuf ok!"); // Print a success message
   }
+}
+
+void countPulses() {
+  pulseCount++;
+}
+
+float getRPM() {
+  int wheelHoles = 20;
+  int rotations = pulseCount / wheelHoles;    // 회전 수 계산 (20개 구멍)
+  float rpm = rotations * 60;                  // 1분당 펄스 수
+
+  return rpm;
 }
